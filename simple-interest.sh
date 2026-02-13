4a941c63a89124f6e612acd4534615e3fc60dfed
Prateek Jha <151683328+prateekjhaa369@users.noreply.github.com>
2026-02-13 14:54:42 +0530

    Simplify comments in simple-interest.sh
    Removed unnecessary comments and author information.

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,20 +1,6 @@
 #!/bin/bash
-# This script calculates simple interest given principal,
-# annual rate of interest and time period in years.
-
-# Do not use this in production. Sample purpose only.
-
-# Author: Prateek Jha
-# Additional Authors:
-# prateekjhaa369
-
-# Input:
-# p, principal amount
-# t, time period in years
-# r, annual rate of interest
-
-# Output:
-# simple interest = p*t*r
+# This script calculates simple interest.
+# Formula: s = (p * t * r) / 100
 
 echo "Enter the principal:"
 read p
